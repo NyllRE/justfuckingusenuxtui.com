7fe59d7244d8dc4e84f359213bc2d9ee184a37e0
barbapapazes <e.soubiran25@gmail.com>
2026-01-26 22:16:31 +0100

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    Just Fucking use Nuxt UI.
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/content'
+  ],
+
+  devtools: {
+    enabled: false
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
+})
